45c5fe4a6d0912f90443d2ffa322dcb8ae0c3c59
zaileshar <zaileshar@gmail.com>
2024-04-12 12:11:56 +0530

    refactor state machine

---
+++ npuacceleratoroptimized_driver.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+class Npuacceleratoroptimized_driver {
+public:
+    void printStatus() {
+        std::cout << "Npuacceleratoroptimized_driver initialized." << std::endl;
+    }
+};
